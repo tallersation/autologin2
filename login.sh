0d4a14b9b70a32c026268b214fa872cec98224a9
tallersation <82475104+tallersation@users.noreply.github.com>
2024-08-27 17:05:29 +0700

    Update login.sh
    CHANGE PARAMETERS

--- login.sh
+++ login.sh
@@ -5,7 +5,7 @@ do
     magic=$(curl -s google.com | cut -d "?" -f 2 | cut -d "\"" -f 1)
     curl -s https://security.eng.kmutnb.ac.th:1003/fgtauth?"$magic"
 
-    resp=$(curl -d --request POST 'https://security.eng.kmutnb.ac.th:1003' --data "username=s6301023611038" --data "password=Koonname@221e" --data "magic=$magic" --data "4Tredir=google.com")
+    resp=$(curl -d --request POST 'https://security.eng.kmutnb.ac.th:1003' --data "username={STUDENT_USERNAME}" --data "password={STUDENT_PASSWORD}" --data "magic=$magic" --data "4Tredir=google.com")
     if [[ $resp == "co cotunue." ]]; then
         echo "Logged in"
     fi
